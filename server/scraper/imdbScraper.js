3fca9302c9910cba6ba161203090fa0575e685a9
santhosapriyan <santhosapriyank@karunya.edu.in>
2026-06-26 14:15:57 +0530

    Fix puppeteer on render

--- server/scraper/imdbScraper.js
+++ server/scraper/imdbScraper.js
@@ -10,7 +10,7 @@ async function scrapeIMDb() {
 
     browser = await puppeteer.launch({
       headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
+      args: ['--no-sandbox', '--disable-setuid-sandbox','--disable-dev-shm-usage', '--disable-gpu'],
     });
 
     const page = await browser.newPage();
